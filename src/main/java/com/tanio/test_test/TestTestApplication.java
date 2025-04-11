79fa14d6ed8ebfe5204246f07df225ab724727e8
tmessina <tanio.messina@8x8.com>
2025-04-11 10:57:11 +0100

    First commit

---
+++ src/main/java/com/tanio/test_test/TestTestApplication.java
@@ -0,0 +1,13 @@
+package com.tanio.test_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanio/test_test/TestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanio.test_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
