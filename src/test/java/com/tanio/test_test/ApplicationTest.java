6ffe8cf43c05704142c839211e74e370087db9b2
rgone <raja.gone@8x8.com>
2025-04-11 11:24:04 +0100

    added new test - whenCallIsNullThenThrowException

--- src/test/java/com/tanio/test_test/ApplicationTest.java
+++ src/test/java/com/tanio/test_test/ApplicationTest.java
@@ -4,12 +4,19 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 @ExtendWith(MockitoExtension.class)
 class ApplicationTest {
 
-    @Test
-    void test() {
+  @Test
+  void whenCallIsNullThenThrowException() {
+    Agent agent = new Agent();
+    Phone phone = new Phone();
+    Application application = new Application(agent, phone);
 
-    }
-
+    assertThatThrownBy(() -> application.receiveCall(null))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("Call cannot be null");
+  }
 }
