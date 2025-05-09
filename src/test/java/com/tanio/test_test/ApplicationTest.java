1f4e7fe6596100a33429ce2272d09812e79e115a
rgone <raja.gone@8x8.com>
2025-05-09 11:30:50 +0100

    use mock annotations

--- src/test/java/com/tanio/test_test/ApplicationTest.java
+++ src/test/java/com/tanio/test_test/ApplicationTest.java
@@ -2,43 +2,40 @@ package com.tanio.test_test;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ApplicationTest {
 
+  @Mock
+  PhoneCall phoneCall;
+  @Mock
+  Phone phone;
+  @Mock
+  Agent agent;
+  @InjectMocks
+  Application sut;
+
   @Test
   void handleNullPhoneCall() {
-    Agent agent = new Agent();
-    Phone phone = new Phone();
-    Application application = new Application(agent, phone);
-    assertThatThrownBy(() -> application.receiveCall(null))
+    assertThatThrownBy(() -> sut.receiveCall(null))
         .isInstanceOf(IllegalArgumentException.class)
         .hasMessage("Call cannot be null").doesNotHaveToString("Baba");
   }
 
   @Test
   void whenCallIsForSupportAndAgentIsSupportThenCallIsAnsweredAndPhoneConnectedToAgent() {
-
-    PhoneCall phoneCall = Mockito.mock(PhoneCall.class);
     when(phoneCall.isForSupport()).thenReturn(true);
+    when(agent.isSupportAgent()).thenReturn(true);
 
-    Phone phone = Mockito.mock(Phone.class);
+    sut.receiveCall(phoneCall);
 
-    Agent mockAgent = Mockito.mock(Agent.class);
-    when(mockAgent.isSupportAgent()).thenReturn(true);
-
-    Application application = new Application(mockAgent, phone);
-
-    application.receiveCall(phoneCall);
-
-    Mockito.verify(phoneCall).answer();
-    Mockito.verify(phone).connectToAgent();
-
+    verify(phoneCall).answer();
+    verify(phone).connectToAgent();
   }
-
 }
